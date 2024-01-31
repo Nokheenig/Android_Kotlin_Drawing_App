d568084a969f59e455b057b654b94dad72b7ecc9
Yoann <yoann.chamillard@gmail.com>
2024-01-31 15:33:14 +0100

    141. Let the drawing persist - Now the drawing persists but with a bug: first stroke is erased

--- app/src/main/java/com/mydomain/drawingapp/DrawingView.kt
+++ app/src/main/java/com/mydomain/drawingapp/DrawingView.kt
@@ -23,6 +23,7 @@ class DrawingView (context: Context, attrs: AttributeSet) : View(context, attrs)
     private lateinit var canvas: Canvas
     private lateinit var canvasBitmap: Bitmap
     private var brushSize: Float = 0.toFloat()
+    private val paths = mutableListOf<FingerPath>()
 
     init {
         setUpDrawing()
@@ -58,6 +59,7 @@ class DrawingView (context: Context, attrs: AttributeSet) : View(context, attrs)
             //this event will be fired when the user will pick up the finger on the screen
             MotionEvent.ACTION_UP -> {
                 drawPath = FingerPath(color, brushSize)
+                paths.add(drawPath)
             }
             else -> return false
         }
@@ -68,11 +70,19 @@ class DrawingView (context: Context, attrs: AttributeSet) : View(context, attrs)
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         canvas?.drawBitmap(canvasBitmap, 0f,0f,drawPaint)
+
+        for (path in paths) {
+            drawPaint.strokeWidth = path.brushThickness
+            drawPaint.color = path.color
+            canvas?.drawPath(path, drawPaint) // drawing path on the canvas
+        }
+
         if(!drawPath.isEmpty){
             drawPaint.strokeWidth = drawPath.brushThickness
             drawPaint.color = drawPath.color
             canvas?.drawPath(drawPath, drawPaint) // drawing path on the canvas
         }
+
     }
 
     private fun setUpDrawing() {
